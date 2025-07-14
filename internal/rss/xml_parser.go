47b1b8d05c9c29f9b3f7af2b5b73527be3f4204d
Andrew-The-Cat <andrew@lekas.tech>
2025-07-14 13:03:59 +0300

    Add XML parser

---
+++ internal/rss/xml_parser.go
@@ -0,0 +1,68 @@
+package rss
+
+import (
+	"bytes"
+	"context"
+	"encoding/xml"
+	"fmt"
+	"html"
+	"io"
+	"net/http"
+)
+
+type RSSFeed struct {
+	Channel struct {
+		Title       string    `xml:"title"`
+		Link        string    `xml:"link"`
+		Description string    `xml:"description"`
+		Item        []RSSItem `xml:"item"`
+	} `xml:"channel"`
+}
+
+type RSSItem struct {
+	Title       string `xml:"title"`
+	Link        string `xml:"link"`
+	Description string `xml:"description"`
+	PubDate     string `xml:"pubDate"`
+}
+
+func FetchFeed(ctx context.Context, feedURL string) (*RSSFeed, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", feedURL, bytes.NewBuffer(make([]byte, 0)))
+	if err != nil {
+		return nil, fmt.Errorf("unable to form the http request: %v", err)
+	}
+
+	req.Header.Set("User-Agent", "gator")
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error when making the request: %v", err)
+	}
+
+	data, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("unable to read response body: %v", err)
+	}
+
+	var result RSSFeed
+	if err := xml.Unmarshal(data, &result); err != nil {
+		return nil, fmt.Errorf("unable to interpret response: %v", err)
+	}
+
+	cleanFeed(&result)
+
+	return &result, nil
+}
+
+func cleanFeed(feed *RSSFeed) error {
+	feed.Channel.Title = html.UnescapeString(feed.Channel.Title)
+	feed.Channel.Description = html.UnescapeString(feed.Channel.Description)
+
+	for _, item := range feed.Channel.Item {
+		item.Description = html.UnescapeString(item.Description)
+		item.Title = html.UnescapeString(item.Title)
+	}
+
+	return nil
+}
