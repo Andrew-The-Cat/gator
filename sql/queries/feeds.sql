6b85495cc3c42ec9abd888ec6e234bbb399afe7e
Andrew-The-Cat <andrew@lekas.tech>
2025-07-17 10:34:22 +0300

    Update schema for fetching

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -21,3 +21,17 @@ WHERE url = $1;
 
 -- name: FeedsReset :exec
 DELETE FROM feeds *;
+
+-- name: MarkFeedFetched :one
+UPDATE feeds
+SET last_fetched_at = $2,
+    updated_at = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+WHERE user_id = $1
+ORDER BY last_fetched_at ASC
+NULLS FIRST
+LIMIT 1;
